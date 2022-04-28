c97dc3c35b8ea261d283dcba83dd94d115fbb494
Nilesh Vishwakarma <nileshv@axioned.com>
2022-04-28 16:55:42 +0530

    added position for duplicates

--- search_repeate_string_char.php
+++ search_repeate_string_char.php
@@ -2,13 +2,26 @@
 
 $string = "kkjnmnmnjjjnmn";
 
+echo "inpu string is : ".$string."<hr>";
+
 $array = array();
 for($i = 0; $i<strlen($string); $i++) {
-    $arr_str[$string[$i]] = $string[$i];
+    for ($j = $i +1; $j < strlen($string); $j++) {
+        if ($string[$i] == $string[$j]) {
+            $position[$string[$i]] = $j;
+        }
+    }
+    $array[$string[$i]] = $string[$i];
 }
 
-foreach($array as $characters) {
-    echo $characters;
+echo "The consecutive repeated characters are ";
+foreach($array as $arr_key => $characters) {
+    echo $characters." ";
+}
+echo "<hr>";
+
+foreach ($position as $pos_key => $pos_num) {
+    echo "The position of index value of a duplicate characters for ".$pos_key." is ".$pos_num."<br>";
 }
 
 ?>
